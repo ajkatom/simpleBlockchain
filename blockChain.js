9f8b2a0619b6bd032deeb15128afa0e750454616
ajkatom <alonadelson@alons-MacBook-Pro.local>
2020-02-22 11:19:43 -0500

    adding new block

--- blockChain.js
+++ blockChain.js
@@ -1,10 +1,24 @@
-import  Block from './block';
+import Block from './block';
 
 export default class BlockChain {
-  constructor(){
-    this.blockChain = [this.firstBlock()]
+  constructor() {
+    this.blockChain = [this.firstBlock()];
+    this.difficulty = 4;
   }
-  firstBlock (){
-    return new Block("First block","0")
+
+  firstBlock() {
+    return new Block('First block', '0');
   }
+
+  getChainHeadBlock() {
+    return this.blockChain[this.blockChain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getChainHeadBlock().hash;
+    newBlock.proofOfWork(this.difficulty);
+    this.blockChain.push(newBlock);
+  }
+
+  
 }
